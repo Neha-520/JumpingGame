89df887eccdd29b9c84d50e7afac9c5aa29a16e0
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-09-28 02:31:01 +0530

    rotating the cube

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d'); //returns  drawing context - which is an object that has all the drawing properties and functions you use to draw on the canvas. 
 
 //SFX
-let scoreSFX = new Audio("https://ia903403.us.archive.org/0/items/classiccoin/classiccoin.mp3")
-let gameOverSFX = new Audio();
-let jumpSFX = new Audio();
+let scoreSFX = new Audio("./audio/classiccoin.mp3")
+let gameOverSFX = new Audio('./audio/jump/mp3');
+let jumpSFX = new Audio('./smb_gameover/mp3');
 
 function drawBackgrounfLine() {
     ctx.beginPath();
@@ -25,7 +25,37 @@ class Player {
         //thse 3 are used for jump configuration
         this.shouldJump = false;
         this.jumpCounter = 0; //allows us to stop animation after 32 frames
+        //spin animation related
+        this.spin = 0;
+        //get perfect 90 degree rotation
+        this.spinIncrement = 90 / 32;
     }
+
+    rotation() {
+        //take center point of square 
+        let offsetXPosition = this.x + (this.size / 2);
+        let offsetYPosition = this.y + (this.size / 2);
+        //move canvas origin to that point and cube will rotate in relation to its center
+        ctx.translate(offsetXPosition, offsetYPosition);
+
+        ctx.rotate(this.spin * Math.PI / 180);
+        ctx.rotate(this.spinIncrement * Math.PI / 180) //convert degree to radians
+        //move the canvas back to its original position 
+        ctx.translate(-offsetXPosition, -offsetYPosition);
+        this.spin += this.spinIncrement;
+
+    }
+
+    counterRotation() {
+        //rotate cube back to its origin so it can be moved upwars properly  
+        let offsetXPosition = this.x + (this.size / 2);
+        let offsetYPosition = this.y + (this.size / 2);
+        ctx.translate(offsetXPosition, offsetYPosition);
+        ctx.rotate(-this.spin * Math.PI / 180);
+        ctx.translate(-offsetXPosition, -offsetYPosition);
+
+    }
+
     jump() {
         if (this.shouldJump) {
             this.jumpCounter++;
@@ -36,15 +66,21 @@ class Player {
                 this.y += 0;
             else if (this.jumpCounter < 33)
                 this.y += this.jumpHeight;
+            this.rotation();
+            //end the cycle
+            if (this.jumpCounter >= 32) {
+                this.counterRotation();
+                this.spin = 0;
+                this.shouldJump = false;
+            }
         }
-        //end the cycle
-        if (this.jumpCounter >= 32) this.shouldJump = false;
     }
 
     draw() {
         this.jump();
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.size, this.size);
+        if (this.shouldJump) this.counterRotation();
     }
 }
 
